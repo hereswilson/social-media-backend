e6eec2a34c37d1af2aa81678f6a565ed18ffbdf1
Wilson <wheres67@gmail.com>
2023-02-20 03:00:38 -0500

    Basic server

--- main.go
+++ main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
 func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", testHandler)
+
+	const addr = "localhost:8080"
+	srv := http.Server{
+		Handler:      mux,
+		Addr:         addr,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	fmt.Printf("Starting server at %s", srv.Addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(200)
+	w.Write([]byte("{}"))
 }
