d70cb7a67afc04de5a6157ab46955a55a1d1cdb2
Wilson <wheres67@gmail.com>
2023-02-20 10:03:34 -0500

    Added CRUD functions for database

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
 	"time"
 )
@@ -59,4 +60,103 @@ func (c Client) EnsureDatabase() error {
 		return c.createDatabase()
 	}
 	return err
+}
+
+func (c Client) updateDatabase(data databaseSchema) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(c.dbPath, jsonData, 0600)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c Client) readDatabase() (databaseSchema, error) {
+	data := databaseSchema{}
+	file, err := os.Open(c.dbPath)
+	if err != nil {
+		return data, err
+	}
+	defer file.Close()
+	err = json.NewDecoder(file).Decode(&data)
+	if err != nil {
+		return data, err
+	}
+	return data, nil
+}
+
+// CreateUser -
+func (c Client) CreateUser(email, password, name string, age int) (User, error) {
+	data, err := c.readDatabase()
+	if err != nil {
+		return User{}, err
+	}
+	user := User{
+		CreatedAt: time.Now().UTC(),
+		Email:     email,
+		Password:  password,
+		Name:      name,
+		Age:       age,
+	}
+	data.Users[email] = user
+	err = c.updateDatabase(data)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+func (c Client) UpdateUser(email, password, name string, age int) (User, error) {
+	data, err := c.readDatabase()
+	if err != nil {
+		return User{}, err
+	}
+	user, ok := data.Users[email]
+	if !ok {
+		return User{}, errors.New("user doesn't exist")
+	}
+
+	user.Password = password
+	user.Name = name
+	user.Age = age
+
+	data.Users[email] = user
+
+	err = c.updateDatabase(data)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
+}
+
+func (c Client) GetUser(email string) (User, error) {
+	data, err := c.readDatabase()
+	if err != nil {
+		return User{}, err
+	}
+	user, ok := data.Users[email]
+	if !ok {
+		return User{}, errors.New("user doesn't exist")
+	}
+	return user, nil
+}
+
+func (c Client) DeleteUser(email string) error {
+	data, err := c.readDatabase()
+	if err != nil {
+		return err
+	}
+	_, ok := data.Users[email]
+	if !ok {
+		return errors.New("user doesn't exist")
+	}
+	delete(data.Users, email)
+	err = c.updateDatabase(data)
+	if err != nil {
+		return err
+	}
+	return nil
 }
